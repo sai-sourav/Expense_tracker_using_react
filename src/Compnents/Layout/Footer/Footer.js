07e9d6798040621b6b847ba5eaa423ce72ee3d73
saisourav <saisourav123@gmail.com>
2023-04-06 15:17:14 +0530

    bug fix

--- src/Compnents/Layout/Footer/Footer.js
+++ src/Compnents/Layout/Footer/Footer.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <Navbar bg="dark" sticky="bottom" expand="lg" variant='dark'>
+        <Navbar bg="dark" fixed="bottom" expand="lg" variant='dark'>
             <Container>
                 <Navbar.Brand as={Link} to="/">Expense Tracker</Navbar.Brand>
                 <Nav className="ms-5 gap-5">
--- src/Compnents/Layout/Layout.js
+++ src/Compnents/Layout/Layout.js
@@ -3,12 +3,13 @@ import Header from "./header/Header";
 import Footer from "./Footer/Footer";
 
 import "./Layout.css"
+import { Container } from "react-bootstrap";
 
 export default function Layout(props) {
   return (
     <>
       <Header />
-      <div className="body-background">{props.children}</div>
+      <Container className="body-background">{props.children}</Container>
       <Footer />
     </>
   );
